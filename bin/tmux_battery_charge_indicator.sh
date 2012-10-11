77da503e884419dac453c33d8071c52660fd1694
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2012-10-10 22:51:17 -0500

    oops, flipped current charge and total charge

--- bin/tmux_battery_charge_indicator.sh
+++ bin/tmux_battery_charge_indicator.sh
@@ -3,8 +3,8 @@
 HEART='♥'
 
 if [[ `uname` == 'Linux' ]]; then
-  current_charge=$(cat /proc/acpi/battery/BAT1/info | grep 'last full capacity' | awk '{print $4}')
-  total_charge=$(cat /proc/acpi/battery/BAT1/state | grep 'remaining capacity' | awk '{print $3}')
+  current_charge=$(cat /proc/acpi/battery/BAT1/state | grep 'remaining capacity' | awk '{print $3}')
+  total_charge=$(cat /proc/acpi/battery/BAT1/info | grep 'last full capacity' | awk '{print $4}')
 else
   battery_info=`ioreg -rc AppleSmartBattery`
   current_charge=$(echo $battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
