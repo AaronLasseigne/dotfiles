f1459d1aa7910411556c5eb9bab02d70e5b18202
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2012-10-10 22:35:09 -0500

    updating battery charge indicator to work on linux and osx

--- bin/tmux_battery_charge_indicator.sh
+++ bin/tmux_battery_charge_indicator.sh
@@ -2,9 +2,14 @@
 
 HEART='♥'
 
-battery_info=`ioreg -rc AppleSmartBattery`
-current_charge=$(echo $battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | cut -d ' ' -f 3)
-total_charge=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | cut -d ' ' -f 3)
+if [[ `uname` == 'Linux' ]]; then
+  current_charge=$(cat /proc/acpi/battery/BAT1/info | grep 'last full capacity' | awk '{print $4}')
+  total_charge=$(cat /proc/acpi/battery/BAT1/state | grep 'remaining capacity' | awk '{print $3}')
+else
+  battery_info=`ioreg -rc AppleSmartBattery`
+  current_charge=$(echo $battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
+  total_charge=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
+fi
 
 charged_slots=$(echo "(($current_charge/$total_charge)*10)+1" | bc -l | cut -d '.' -f 1)
 if [[ $charged_slots -gt 10 ]]; then
