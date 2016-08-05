f0c0be7aa5efb1a7f1779bf106f5bbb029fe93d1
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2016-08-05 13:14:25 -0500

    check for plugins and install if missing

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,11 @@
+# install plugins
+set plugins segment
+for plugin in plugins
+  if [ (fisher ls | grep $plugin) ]
+    fisher $plugin
+  end
+end
+
 set PATH /usr/local/opt/rbenv/shims /usr/local/opt/rbenv/bin /usr/local/bin $PATH
 
 # for rbenv
