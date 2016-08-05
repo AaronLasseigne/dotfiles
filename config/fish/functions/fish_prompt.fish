13e36a45f5b5aa4ca075123e51bb831efc8bde48
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2016-08-05 12:52:43 -0500

    show when a git branch is dirty

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,25 +1,35 @@
 function fish_prompt --description 'My custom prompt'
   set last_status $status
 
-  set git_root (git rev-parse --show-toplevel ^/dev/null)
-  if [ "$git_root" ]
+  # in a git directory
+  if [ (git rev-parse --show-toplevel ^/dev/null) ]
+    set dirty_flag;
+    if [ (git diff --shortstat ^/dev/null | tail -n1) ]
+      set dirty_flag '+ '
+    end
+
     set ref (git symbolic-ref HEAD ^/dev/null)
     if [ $status -ne 0 ]
+      # print ref info
       set ref (git show-ref --head -s --abbrev | head -n1)
-      segment 424242 fdd835 " \uE0A0 $ref "
+      segment 424242 fdd835 " \uE0A0 $ref $dirty_flag"
     else
+      # print branch info
       set ref (echo -n $ref | sed "s#refs/heads/##")
-      segment eee 1b5e20 " \uE0A0 $ref "
+      segment eee 1b5e20 " \uE0A0 $ref $dirty_flag"
     end
   end
 
+  # current directory
   set current_dir (prompt_pwd)
   segment bdbdbd 424242 " $current_dir "
 
+  # background jobs
   if [ (jobs -l | wc -l) -gt 0 ]
     segment eee ef6c00 ' %% '
   end
 
+  # failed exit
   if [ $last_status -ne 0 ]
     segment eee b71c1c ' ! '
   end
