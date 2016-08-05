b2d3ab19e95d096370312e36862753f711af4345
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2016-08-05 13:15:27 -0500

    only do this if rbenv is installed

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -9,4 +9,6 @@ end
 set PATH /usr/local/opt/rbenv/shims /usr/local/opt/rbenv/bin /usr/local/bin $PATH
 
 # for rbenv
-status --is-interactive; and . (rbenv init -|psub)
+if [ (which rbenv) ]
+  status --is-interactive; and . (rbenv init -|psub)
+end
