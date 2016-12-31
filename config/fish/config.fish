822b32506af7a35d69e70a7ebac74b2c43e2f670
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2016-12-30 20:06:18 -0600

    use a preview when searching files with fzf

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,6 @@
 set -U EDITOR 'nvim -p'
 set -U FZF_DEFAULT_COMMAND 'rg --smart-case --files --no-ignore --hidden --follow --glob "!.git/*"'
+set -U FZF_DEFAULT_OPTS '--preview "begin; cat {}; or tree -C {}; end 2> /dev/null | head -200"'
 
 # install plugins
 set plugins segment
