6adf6d07147f2917e75bc53093a70aba67d949b6
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2018-07-31 22:21:23 -0500

    fix caps lock remapped ctrl

--- .hammerspoon/modules/double_bind_modifier_keys.lua
+++ .hammerspoon/modules/double_bind_modifier_keys.lua
@@ -2,18 +2,23 @@ function doubleBindModifierKey(modName, modifiers, key)
   local modToCode = {
     ['left shift'] = 56,
     ['ctrl'] = 59,
-    ['right shift'] = 60
+    ['right shift'] = 60,
+    ['left ctrl'] = 62
   }
   local codeToMod = {
     [56] = 'shift', -- left
     [59] = 'ctrl',
-    [60] = 'shift' -- right
+    [60] = 'shift', -- right
+    [62] = 'ctrl' -- caps lock remapped via the system
   }
 
   local ms_wait = 150
   local ns_wait = ms_wait * 1000000
 
   local keyDownAt = 0
+  hs.eventtap.new({hs.eventtap.event.types.keyDown}, function(eventType)
+    keyDownAt = 0
+  end):start()
   hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, function(eventType)
     local keyCode = modToCode[modName]
 
@@ -32,5 +37,6 @@ function doubleBindModifierKey(modName, modifiers, key)
   end):start()
 end
 doubleBindModifierKey('ctrl', {}, 'escape')
+doubleBindModifierKey('left ctrl', {}, 'escape')
 doubleBindModifierKey('left shift', {'shift'}, '9')
 doubleBindModifierKey('right shift', {'shift'}, '0')
