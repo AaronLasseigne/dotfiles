bbcfcd42d1ea9c3040a1579d65bef1e57c0d5450
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2019-03-04 14:02:10 -0600

    add RubyMotion envs and remove fisher plugin installation

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,13 +1,9 @@
 set -x EDITOR 'nvim -p'
 set -x FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
 set -x FZF_DEFAULT_OPTS '--preview "begin; cat {}; or tree -C {}; end 2> /dev/null | head -200"'
+set -x RUBYMOTION_ANDROID_NDK '/Users/aaron/.rubymotion-android/ndk'
+set -x RUBYMOTION_ANDROID_SDK '/Users/aaron/.rubymotion-android/sdk'
 
 # install plugins
-set plugins 'franciscolourenco/done' 'daenney/rbenv' 'jorgebucaran/fish-segment'
-for plugin in $plugins
-  if [ -z (fisher ls | grep $plugin) ]
-    fisher add $plugin
-  end
-end
 
 set PATH ~/bin (yarn global bin) $PATH
