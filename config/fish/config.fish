5f63aa4938cd75c7d5dd91fe2d8bf42312eba088
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2019-05-01 15:18:04 -0500

    yarn does not load the correct directory unless asdf is loaded first

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,5 +4,5 @@ set -x FZF_DEFAULT_OPTS '--preview "begin; cat {}; or tree -C {}; end 2> /dev/nu
 set -x RUBYMOTION_ANDROID_NDK '/Users/aaron/.rubymotion-android/ndk'
 set -x RUBYMOTION_ANDROID_SDK '/Users/aaron/.rubymotion-android/sdk'
 
-set PATH ~/bin (yarn global bin) $PATH
 source (brew --prefix asdf)/asdf.fish
+set PATH ~/bin (yarn global bin) $PATH
