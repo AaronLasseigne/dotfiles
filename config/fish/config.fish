a7c5f1fc4b93767e0b52873977b95fc9b895b009
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2019-11-06 19:51:50 -0600

    use bat for FZF previews

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,6 @@
 set -x EDITOR 'nvim -p'
 set -x FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
-set -x FZF_DEFAULT_OPTS '--preview "begin; cat {}; or tree -C {}; end 2> /dev/null | head -200"'
+set -x FZF_DEFAULT_OPTS '--preview "begin; bat {} --color=always; or tree -C {}; end 2> /dev/null | head -200"'
 set -x RUBYMOTION_ANDROID_NDK '/Users/aaron/.rubymotion-android/ndk'
 set -x RUBYMOTION_ANDROID_SDK '/Users/aaron/.rubymotion-android/sdk'
 
