46786546075a6a72d45e89f019ba4d937d04ec67
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2020-04-28 14:07:34 -0500

    suggested by brew doctor

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,4 +7,4 @@ set -x RUBYMOTION_ANDROID_SDK '/Users/aaron/.rubymotion-android/sdk'
 source (brew --prefix asdf)/asdf.fish
 set PATH ~/bin (yarn global bin) $PATH
 # to put brew installs before system installs
-set -g fish_user_paths "/usr/local/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/bin" "/usr/local/sbin" $fish_user_paths
