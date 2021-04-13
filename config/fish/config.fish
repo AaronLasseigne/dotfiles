d29e1a090a4c0055af4b882d01af1fe1b41c86ba
Aaron Lasseigne <aaron@modeanalytics.com>
2021-04-13 15:52:26 -0500

    I don't seem to need this anymore

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,8 +5,6 @@ set -x FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.g
 set -x FZF_DEFAULT_OPTS '--preview "begin; bat {} --color=always; or tree -C {}; end 2> /dev/null | head -200"'
 
 source /usr/local/opt/asdf/asdf.fish
-# to put brew installs before system installs
-set -g fish_user_paths "/usr/local/bin" "/usr/local/sbin" $fish_user_paths
 
 if test -e ~/.config/fish/config.local.fish
   source ~/.config/fish/config.local.fish
