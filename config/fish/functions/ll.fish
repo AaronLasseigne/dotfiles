4acbd02a27c4d964bc30626f34164586083ff294
Aaron Lasseigne <aaron@modeanalytics.com>
2021-06-17 13:49:21 -0500

    give exa a try

--- config/fish/functions/ll.fish
+++ config/fish/functions/ll.fish
@@ -1,3 +1,3 @@
-function ll --wraps ls --description 'List contents of directory using long format and human readable sizes'
-  ls -lho $argv 
+function ll --wraps exa --description 'List contents of directory using long format and human readable sizes'
+  exa -l --group-directories-first --no-user $argv
 end
