c59725b13d9312019dd2b788d308c683f5214c6f
Aaron Lasseigne <aaron@modeanalytics.com>
2022-01-06 17:03:30 -0600

    hammerspoon is too finicky for this, use karabiner elements

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,15 +1,1 @@
-local function reloadConfig(files)
-  local doReload = false
-  for _,file in pairs(files) do
-    if file:sub(-4) == ".lua" then
-      doReload = true
-    end
-  end
-  if doReload then
-    hs.reload()
-  end
-end
-hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
-
-require('modules/double_bind_modifier_keys')
 require('modules/window_manager')
