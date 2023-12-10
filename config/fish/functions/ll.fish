927a493af439516a43bfba265ffec404a29a0fff
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2023-12-10 15:26:52 -0600

    fix ll

--- config/fish/functions/ll.fish
+++ config/fish/functions/ll.fish
@@ -1,3 +1,3 @@
-function ll --wraps exa --description 'List contents of directory using long format and human readable sizes'
-  exa -l --group-directories-first --no-user $argv
+function ll --wraps eza --description 'List contents of directory using long format and human readable sizes'
+  eza -l --group-directories-first --no-user $argv
 end
