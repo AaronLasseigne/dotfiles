f33035496f40920a2f26de0018f2505e303f3368
Aaron Lasseigne <aaron@mode.com>
2024-01-19 10:59:04 -0600

    update to use eza instead of exa

--- config/fish/functions/l..fish
+++ config/fish/functions/l..fish
@@ -1,6 +1,6 @@
 function l. --description 'List dotfiles in a directory using long format'
   set files .*
   if [ (count $files) -gt 0 ]
-    exa -l --group-directories-first --no-user --all --ignore-glob='[a-zA-Z0-9]*'
+    eza -l --group-directories-first --no-user --all --ignore-glob='[a-zA-Z0-9]*'
   end
 end
