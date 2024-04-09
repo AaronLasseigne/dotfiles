b89fbbbd7ea3891d0b15894ccc6c4a80d1c02c43
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2024-04-08 19:49:42 -0500

    add searching for the highlighted selection

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -266,9 +266,10 @@ require('lazy').setup({
       dependencies = { 'nvim-lua/plenary.nvim' },
       init = function()
         local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+        vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
       end,
       config = function()
