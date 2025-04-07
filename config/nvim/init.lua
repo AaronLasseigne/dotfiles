bf9d629237309c20c29f11ae732a7606d9e266d1
Aaron Lasseigne <aaron@mode.com>
2025-04-07 16:10:37 -0500

    make the relative file path show

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -108,6 +108,9 @@ require('lazy').setup({
         require('lualine').setup {
           options = {
             theme = 'gruvbox'
+          },
+          sections = {
+            lualine_c = { { 'filename', path = 1 } }
           }
         }
       end
