fa805bbd723331bc6ad99792d7b2e9842272f3ea
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2025-07-11 14:53:49 -0500

    update asdf setup for new version

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,7 +7,19 @@ set -x FZF_DEFAULT_OPTS '--preview "begin; bat --color=always --style=numbers {}
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+  set _asdf_shims "$HOME/.asdf/shims"
+else
+  set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+  set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
 
 fish_add_path ~/bin
 
