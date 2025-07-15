a31b5a738a5ea71aa2c15648521d4629bbbd86ab
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2025-07-15 11:37:54 -0500

    add diagnostic support

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -211,10 +211,14 @@ require('lazy').setup({
 
         -- Global mappings.
         -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-        -- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
-        -- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-        -- vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-        -- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+        vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float)
+        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+        vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+        vim.keymap.set('n', '<leader>dl', vim.diagnostic.setloclist)
+
+        vim.diagnostic.config({
+          virtual_text = true -- show diagnostics
+        })
 
         -- Use LspAttach autocommand to only map the following keys
         -- after the language server attaches to the current buffer
