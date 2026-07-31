a8ae5acb0deb897521a585f955ef2ad09db75985
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2026-07-31 10:05:20 -0500

    add Mason to the title screen

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -116,6 +116,7 @@ local packages = {
             { icon = ' ', key = 'g', desc = 'Find Text', action = ':lua Snacks.dashboard.pick("live_grep")' },
             { icon = ' ', key = 'r', desc = 'Recent Files', action = ':lua Snacks.dashboard.pick("oldfiles")' },
             { icon = '󰒲 ', key = 'L', desc = 'Lazy', action = ':Lazy'},
+            { icon = 'M ', key = 'M', desc = 'Mason', action = ':Mason'},
             { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
           }
         }
